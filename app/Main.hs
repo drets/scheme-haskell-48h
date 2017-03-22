c2039c297a59db9ab353afb7c476066ce3fccd5a
Dmitry Rets <dmitryrets@gmail.com>
2017-03-22 21:52:21 +0200

    Build repl.
    https://en.wikibooks.org/wiki/Write_Yourself_a_Scheme_in_48_Hours/Building_a_REPL

--- app/Main.hs
+++ app/Main.hs
@@ -2,6 +2,7 @@ module Main where
 
 import Control.Monad
 import System.Environment
+import System.IO
 
 import Scheme.Eval
 import Scheme.Parser
@@ -9,5 +10,29 @@ import Scheme.Parser
 main :: IO ()
 main = do
   args <- getArgs
-  evaled <- return $ liftM show $ readExpr (args !! 0) >>= eval
-  putStrLn $ extractValue $ trapError evaled
+  case length args of
+    0 -> runRepl
+    1 -> evalAndPrint $ args !! 0
+    _ -> putStrLn "Program takes only 0 or 1 arguments"
+
+flushStr :: String -> IO ()
+flushStr str = putStr str >> hFlush stdout
+
+readPromt :: String -> IO String
+readPromt prompt = flushStr prompt >> getLine
+
+evalString :: String -> IO String
+evalString expr = return $ extractValue $ trapError (liftM show $ readExpr expr >>= eval)
+
+evalAndPrint :: String -> IO ()
+evalAndPrint expr = evalString expr >>= putStrLn
+
+until_ :: Monad m => (a -> Bool) -> m a -> (a -> m ()) -> m ()
+until_ pred prompt action = do
+  result <- prompt
+  if pred result
+    then return ()
+    else action result >> until_ pred prompt action
+
+runRepl :: IO ()
+runRepl = until_ (== "quit") (readPromt "Lisp>>> ") evalAndPrint
